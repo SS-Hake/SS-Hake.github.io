f137fcac17f733df8e8deef862daa1e42cc33928
Sam Scott-Hake <sam_hake@live.co.uk>
2015-05-28 16:41:22 +0100

    Add missing angular test code.

--- assets/javascript/angular-test.js
+++ assets/javascript/angular-test.js
@@ -7,8 +7,22 @@
 	  $interpolateProvider.endSymbol(']]');
 	});
 
-	function MyCtrl($scope) {
-	  $scope.name = 'Clark Kent';
-	}
+	app.controller('directoryController', function() {
+
+		var dirList = this;
+
+		dirList.list1 = [
+			{name: 'Sam', age:'23'},
+			{name: 'Courtney', age:'23'},
+			{name: 'John', age: '32'}, 
+			{name: 'Jenny', age: '25'}
+		];
+
+		dirList.addPerson = function() {
+			dirList.list1.push({name: dirList.name, age: dirList.age});
+			dirList.name = '';
+			dirList.age = null;
+		};
+	});
 
 //});
