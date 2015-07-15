3d4967c6e06060a24a747f9f1d3b839645317192
Sam Scott-Hake <sam_hake@live.co.uk>
2015-07-16 00:08:50 +0100

    Add all wireframes.

--- assets/javascript/threejs.js
+++ assets/javascript/threejs.js
@@ -22,6 +22,7 @@ var radius = 50,
 	segments = 16,
 	rings = 16;
 
+var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
 	viewAngle,
 	aspect,
@@ -29,21 +30,24 @@ var camera = new THREE.PerspectiveCamera(
 	far
 );
 
-var scene = new THREE.Scene();
+camera.position.x = -30;
+camera.position.y = 40;
+camera.position.z = 30;
+camera.lookAt(scene.position);
+
 var renderer = new THREE.WebGLRenderer();
-var sphereMaterial = 
+/*var sphereMaterial = 
 	new THREE.MeshLambertMaterial(
 	{
 		color: 0xCC0000
 	});
-
+*/
 renderer.setSize(width, height);
-camera.position.z = 300;
-scene.add(camera);
+
 document.body.appendChild(renderer.domElement);
 
 var axes = new THREE.AxisHelper(20);
-scene.add(aces);
+scene.add(axes);
 
 //Plane - ground
 var planeGeometry = new THREE.PlaneGeometry(60, 20);
@@ -51,13 +55,35 @@ var planeMaterial = new THREE.MeshBasicMaterial({color: 0xcccccc});
 var plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
 plane.rotation.x = -0.5 * Math.PI;
-plane.rotation.x = 15;
-plane.rotation.y = 0;
-plane.rotation.z = 0;
+plane.position.x = 15;
+plane.position.y = 0;
+plane.position.z = 0;
 
 scene.add(plane);
 
-var sphere = new THREE.Mesh(
+//Cube
+var cubeGeometry = new THREE.CubeGeometry(4, 4, 4);
+var cubeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
+var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+cube.position.x = -4;
+cube.position.y = 3;
+cube.position.z = 0;
+
+scene.add(cube);
+
+var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+var sphereMaterial = new THREE.MeshBasicMaterial({color: 0x7777ff, wireframe: true});
+var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+
+sphere.position.x = 20;
+sphere.position.y = 4;
+sphere.position.z = 2;
+
+scene.add(sphere);
+
+//scene.add(sphere);
+/*var sphere = new THREE.Mesh(
 	new THREE.SphereGeometry(
 		radius,
 		segments,
@@ -65,8 +91,7 @@ var sphere = new THREE.Mesh(
 	),
 sphereMaterial);
 
-sphere.scale.x = 1;
-scene.add(sphere);
+sphere.scale.x = 1;*/
 
 var pointLight = new THREE.PointLight(0xFFFFFF);
 pointLight.position.x = 10;
